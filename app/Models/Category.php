094b04f1ec3cc12f302dbc96708373a3d86a8040
tintasetons <tintasetons@gmail.com>
2019-07-28 04:29:43 -0300

    Criando estrutura para categorias

---
+++ app/Models/Category.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CodeFin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+}
---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(CodeFin\Models\Category::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
